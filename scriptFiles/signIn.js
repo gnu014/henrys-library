287f8e78b4b46c1533d00bb49a766da9fb5858f9
gnu014 <72946332+gnu014@users.noreply.github.com>
2021-05-12 13:35:42 -0700

    Delete scriptFiles directory

--- scriptFiles/signIn.js
+++
@@ -1,8 +0,0 @@
-//popup form
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
